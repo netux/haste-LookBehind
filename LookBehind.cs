cb206b0524339c85861cf449cdb22ff485c80429
Sergey Syrtsev <syrtsevser@gmail.com>
2025-05-22 21:06:33 +0300

    Updated for 1.3.b, resharper suggestions.

--- LookBehind.cs
+++ LookBehind.cs
@@ -7,52 +7,52 @@ namespace HasteLookBehindMod;
 [LandfallPlugin]
 public class LookBehindMod
 {
-    public static InputAction? LookBehindInputAction;
+	public static InputAction? LookBehindInputAction;
 
-    static LookBehindMod()
-    {
-        On.HasteSettingsHandler.ctor += (original, self) =>
-        {
-            original(self);
+	static LookBehindMod()
+	{
+		On.HasteSettingsHandler.ctor += (original, self) =>
+		{
+			original(self);
 
-            LookBehindInputAction = Utils.AddInputAction(
-                actionName: $"{typeof(LookBehindMod).Name}--LookBehind",
-                keyboardBinding: new InputBinding
-                {
-                    id = new Guid("1006be41-9d00-6b00-0000-000000000000"),
-                    path = "<Keyboard>/q",
-                    interactions = "hold(duration=0)"
-                },
-                gamepadBinding: new InputBinding
-                {
-                    id = new Guid("1006be41-9d00-6900-0000-000000000000"),
-                    path = "<GamePad>/rightStickPress",
-                    interactions = "hold(duration=0)"
-                }
-            );
+			LookBehindInputAction = Utils.AddInputAction(
+				actionName: $"{nameof(LookBehindMod)}--LookBehind",
+				keyboardBinding: new InputBinding
+				{
+					id = new Guid("1006be41-9d00-6b00-0000-000000000000"),
+					path = "<Keyboard>/q",
+					interactions = "hold(duration=0)"
+				},
+				gamepadBinding: new InputBinding
+				{
+					id = new Guid("1006be41-9d00-6900-0000-000000000000"),
+					path = "<GamePad>/rightStickPress",
+					interactions = "hold(duration=0)"
+				}
+			);
 
-            Utils.AddLocalizedString("Settings", LookBehindInputAction.name, "Look Behind");
+			Utils.AddLocalizedString("Settings", LookBehindInputAction.name, "Look Behind");
 
-            self.AddSetting(new InputRebindSetting(LookBehindInputAction));
-        };
+			self.AddSetting(new InputRebindSetting(LookBehindInputAction));
+		};
 
-        On.CameraMovement.ApplyRotation += (original, self) =>
-        {
-            original(self);
+		On.CameraMovement.ApplyRotation += (original, self, lookRotationEulerAngles) =>
+		{
+			original(self, lookRotationEulerAngles);
 
-            if (LookBehindInputAction == null)
-            {
-                return;
-            }
+			if (LookBehindInputAction == null)
+			{
+				return;
+			}
 
-            if (LookBehindInputAction.IsInProgress())
-            {
-                self.transform.localEulerAngles = new Vector3(
-                    self.transform.localEulerAngles.x,
-                    self.transform.localEulerAngles.y + 180f,
-                    self.transform.localEulerAngles.z
-                );
-            }
-        };
-    }
+			if (LookBehindInputAction.IsInProgress())
+			{
+				self.transform.localEulerAngles = new Vector3(
+					self.transform.localEulerAngles.x,
+					self.transform.localEulerAngles.y + 180f,
+					self.transform.localEulerAngles.z
+				);
+			}
+		};
+	}
 }
--- Utils.cs
+++ Utils.cs
@@ -5,78 +5,78 @@ namespace HasteLookBehindMod;
 
 internal class Utils
 {
-    public static InputAction AddInputAction(string actionName, InputBinding keyboardBinding, InputBinding gamepadBinding, bool force = false)
-    {
-        if (GetInputBindingIdInternal(keyboardBinding) == null)
-        {
-            throw new ArgumentException("Keyboard Binding is missing an id. Id's are required for persisting binding changes!", "keyboardBinding");
-        }
-        if (GetInputBindingIdInternal(gamepadBinding) == null)
-        {
-            throw new ArgumentException("Gamepad Binding is missing an id. Id's are required for persisting binding changes!", "gamepadBinding");
-        }
+	public static InputAction AddInputAction(string actionName, InputBinding keyboardBinding, InputBinding gamepadBinding, bool force = false)
+	{
+		if (GetInputBindingIdInternal(keyboardBinding) == null)
+		{
+			throw new ArgumentException("Keyboard Binding is missing an id. Id's are required for persisting binding changes!", nameof(keyboardBinding));
+		}
+		if (GetInputBindingIdInternal(gamepadBinding) == null)
+		{
+			throw new ArgumentException("Gamepad Binding is missing an id. Id's are required for persisting binding changes!", nameof(gamepadBinding));
+		}
 
-        if (keyboardBinding.groups != null)
-        {
-            throw new ArgumentException("Keyboard Binding cannot have a group, as it will be overwritten", "keyboardBinding");
-        }
+		if (keyboardBinding.groups != null)
+		{
+			throw new ArgumentException("Keyboard Binding cannot have a group, as it will be overwritten", nameof(keyboardBinding));
+		}
 
-        if (gamepadBinding.groups != null)
-        {
-            throw new ArgumentException("Gamepad Binding cannot have a group, as it will be overwritten", "gamepadBinding");
-        }
+		if (gamepadBinding.groups != null)
+		{
+			throw new ArgumentException("Gamepad Binding cannot have a group, as it will be overwritten", nameof(gamepadBinding));
+		}
 
-        var defaultInputActionMap = Zorro.ControllerSupport.InputHandler.Instance.Default;
+		var defaultInputActionMap = Zorro.ControllerSupport.InputHandler.Instance.Default;
 
-        // "CaNnOt AdD, rEmOvE, oR cHaNgE eLeMeNtS oF iNpUtAcTiOnAsSeT hAsTeInPuTaCtIoNs (UnItYeNgInE.iNpUtSyStEm.InPuTaCtIoNaSsEt) WhIlE oNe Or MoRe Of ItS aCtIoNs ArE eNaBlEd"
-        foreach (var actionMap in defaultInputActionMap.asset.actionMaps)
-        {
-            actionMap.Disable();
-        }
-        defaultInputActionMap.Disable();
+		// "CaNnOt AdD, rEmOvE, oR cHaNgE eLeMeNtS oF iNpUtAcTiOnAsSeT hAsTeInPuTaCtIoNs (UnItYeNgInE.iNpUtSyStEm.InPuTaCtIoNaSsEt) WhIlE oNe Or MoRe Of ItS aCtIoNs ArE eNaBlEd"
+		foreach (var actionMap in defaultInputActionMap.asset.actionMaps)
+		{
+			actionMap.Disable();
+		}
+		defaultInputActionMap.Disable();
 
-        var oldInputAction = defaultInputActionMap.FindAction(actionName);
-        if (oldInputAction != null)
-        {
-            if (!force)
-            {
-                return oldInputAction;
-            }
+		var oldInputAction = defaultInputActionMap.FindAction(actionName);
+		if (oldInputAction != null)
+		{
+			if (!force)
+			{
+				return oldInputAction;
+			}
 
-            oldInputAction.RemoveAction();
-        }
+			oldInputAction.RemoveAction();
+		}
 
-        var newInputAction = defaultInputActionMap.AddAction(actionName);
+		var newInputAction = defaultInputActionMap.AddAction(actionName);
 
-        // Probably not the best to override this without informing the user of the method, but whatever.
-        keyboardBinding.groups = ";Keyboard&Mouse";
-        gamepadBinding.groups = ";Gamepad";
+		// Probably not the best to override this without informing the user of the method, but whatever.
+		keyboardBinding.groups = ";Keyboard&Mouse";
+		gamepadBinding.groups = ";Gamepad";
 
-        newInputAction.AddBinding(keyboardBinding);
-        newInputAction.AddBinding(gamepadBinding);
+		newInputAction.AddBinding(keyboardBinding);
+		newInputAction.AddBinding(gamepadBinding);
 
-        foreach (var actionMap in defaultInputActionMap.asset.actionMaps)
-        {
-            actionMap.Enable();
-        }
-        defaultInputActionMap.Enable();
+		foreach (var actionMap in defaultInputActionMap.asset.actionMaps)
+		{
+			actionMap.Enable();
+		}
+		defaultInputActionMap.Enable();
 
-        return newInputAction;
-    }
+		return newInputAction;
+	}
 
-    /*
-     * Get internal InputBinding ID value because the public `id` field's getter generates an id one is not already present.
-     */
-    private static string? GetInputBindingIdInternal(InputBinding inputBinding)
-    {
-        var idField = typeof(InputBinding).GetField("m_Id", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-        return (string?) idField.GetValue(inputBinding);
-    }
+	/*
+	 * Get internal InputBinding ID value because the public `id` field's getter generates an id one is not already present.
+	 */
+	private static string? GetInputBindingIdInternal(InputBinding inputBinding)
+	{
+		var idField = typeof(InputBinding).GetField("m_Id", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+		return idField?.GetValue(inputBinding) as string;
+	}
 
-    public static void AddLocalizedString(string table, string key, string localized)
-    {
-        LocalizationSettings.StringDatabase
-            .GetTable(table)
-            .AddEntry(key, localized);
-    }
+	public static void AddLocalizedString(string table, string key, string localized)
+	{
+		LocalizationSettings.StringDatabase
+			.GetTable(table)
+			.AddEntry(key, localized);
+	}
 }
